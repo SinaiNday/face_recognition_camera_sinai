eb1a73a8cfe3df1d96d46a6075739a08733f1a51
Nday Sinai <33591892+orville92@users.noreply.github.com>
2018-06-12 01:56:29 +0900

    Update facerec_from_webcam.py

--- facerec_from_webcam.py
+++ facerec_from_webcam.py
@@ -1,5 +1,7 @@
 import face_recognition
 import cv2
+import keras as kr
+import tensorflow as tf
 # ------------------------------------------------------------/
 # Machine Learning face recognition from webcam. by Sinai Nday Kabulo
 # ------------------------------------------------------------/
